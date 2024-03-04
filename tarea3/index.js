c2a84d7fd602059c2addfa682fe875e6fb6820cf
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-04 20:47:10 -0300

    codifica js
    codifica la funcion para que se generen formularios de cada clase del curso, asi el cliente pueda poner la duracion

---
+++ tarea3/index.js
@@ -0,0 +1,26 @@
+const btnGenerarFormularios = document.querySelector('#btn-generar-formulario-2')
+let btnCalcularDuracion = document.querySelector('#calcular-duracion')
+
+btnGenerarFormularios.onclick = function(){
+    let cantidadDeClases = Number(document.querySelector('#cantidad-de-clases').value)
+
+    for(i = 1; i<=cantidadDeClases; i++){
+        let formulario2 = document.querySelector('#formulario-2')
+
+        formulario2.innerHTML += `<h2>Ingrese los datos de la clase ${i}</h2>
+        <label for="horas-clase-${i}">Ingrese la cantidad de horas de la clase ${i}</label>
+        <input type="number" min="0" id="horas-clase-${i} required">
+        <br>
+        <label for="minutos-clase-${i}">Ingrese la cantidad de minutos de la clase ${i}</label>
+        <input type="number" min="0" max="59" id="minutos-clase-${i} required">
+        <br>
+        <label for="segundos-clase-${i}">Ingrese la cantidad de segundos de la clase ${i}</label>
+        <input type="number" min="0" max="59" id="segundos-clase-${i} required">
+        <br>`
+    }
+
+    btnCalcularDuracion.innerHTML = `<button id="btn-calcular-duracion">Calcular duracion del curso</button>`
+    btnCalcularDuracion = document.querySelector('#btn-calcular-duracion')
+    
+    return false
+}
