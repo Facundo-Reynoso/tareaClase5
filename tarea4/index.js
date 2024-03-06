7657b493aebcfa4ea8cabdbd33e2acdab8eea5eb
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-06 20:56:42 -0300

    crea función analizarPromedio

--- tarea4/index.js
+++ tarea4/index.js
@@ -1,11 +1,20 @@
 //Generar numeros al azar de 1 al 10
 
 let numeroAleatorio = document.querySelectorAll('li')
+let numeros = []
 
 for(i = 0; i < numeroAleatorio.length; i++){
-    numeroAleatorio[i].innerText = Math.floor(Math.random() * 10) + 1
+    numeroAleatorio[i].textContent = Math.floor(Math.random() * 10) + 1
+    numeros.push(Number(numeroAleatorio[i].textContent))
 }
 
 const btnAnalizar = document.querySelector('#boton-analizar')
 
-
+function analizarPromedio(){
+    let acumulador = 0
+    for(i = 0; i < numeros.length; i++){
+        acumulador += numeros[i]
+    }
+    let promedio = acumulador / numeros.length
+    return promedio
+}
