028e088ca49b13a2fe7da836c538c3d24753df50
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-06 20:25:01 -0300

    genera numeros al azar
    en index borro las clases porque no hacían falta

---
+++ tarea4/index.js
@@ -0,0 +1,7 @@
+//Generar numeros al azar de 1 al 10
+
+let numeroAleatorio = document.querySelectorAll('li')
+
+for(i = 0; i < numeroAleatorio.length; i++){
+    numeroAleatorio[i].innerText = Math.floor(Math.random() * 10) + 1
+}
