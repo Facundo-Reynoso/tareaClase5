a15e35f5e40e9e812c4cec8213ea6ffbfa27c774
Facundo Reynoso <reynosofacundo1@hotmail.com>
2024-03-06 20:29:37 -0300

    vincula variable al boton

--- tarea4/index.js
+++ tarea4/index.js
@@ -5,3 +5,7 @@ let numeroAleatorio = document.querySelectorAll('li')
 for(i = 0; i < numeroAleatorio.length; i++){
     numeroAleatorio[i].innerText = Math.floor(Math.random() * 10) + 1
 }
+
+const btnAnalizar = document.querySelector('#boton-analizar')
+
+
